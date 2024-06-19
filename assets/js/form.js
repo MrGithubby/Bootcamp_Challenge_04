e9a947fe0b44d18bfc23f3f6ef526c5907f1b761
cyrous <cybortey@gmail.com>
2024-06-19 15:19:13 -0400

    Initial Working JS

---
+++ assets/js/form.js
@@ -0,0 +1,27 @@
+let blogForm = document.querySelector('form');
+let formUsername = document.querySelector('#username');
+let formTitle = document.querySelector('#title');
+let formContent = document.querySelector('#content');
+let formObject = {
+    username: "",
+    title: "",
+    content: ""
+}
+
+let formArray = []
+
+blogForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    let formObject = {
+        username: "",
+        title: "",
+        content: ""
+    }
+    formObject.username = formUsername.value;
+    formObject.title = formTitle.value;
+    formObject.content = formContent.value;
+    formArray.push(formObject);
+    console.log(blogForm);
+    console.log(formObject.username);
+    console.log(formArray)
+})
