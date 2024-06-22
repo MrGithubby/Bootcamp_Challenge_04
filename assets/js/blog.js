b8157fcfb1e4e3c35cde5994e8cf98ae8a4849b6
cyrous <cybortey@gmail.com>
2024-06-22 16:30:18 -0400

    Initial set for Dark Mode

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -4,19 +4,23 @@ let blogEntries = document.querySelector(".blog-entries");
 const formArray = JSON.parse(localStorage.getItem('formArray'));
 
 for(let i = 0; i < formArray.length; i++){
+    let entryCard = document.createElement('div')
+    entryCard.className = "entryCard"
+
     let blogUsername = document.createElement("p");
     blogUsername.classList.add('blog-username');
     blogUsername.textContent = formArray[i]['username'];
 
-    let blogTitle = document.createElement("h2");
-    blogUsername.classList.add('blog-username');
+    let blogTitle = document.createElement("p");
+    blogTitle.classList.add('blog-title');
     blogTitle.textContent = formArray[i]['title'];
 
     let blogContent = document.createElement("p");
-    blogUsername.classList.add('blog-username');
+    blogContent.classList.add('blog-content');
     blogContent.textContent = formArray[i]['content'];
-
-    blogEntries.append(blogUsername, blogTitle, blogContent);
+    
+    entryCard.append(blogTitle, blogContent, blogUsername);
+    blogEntries.append(entryCard);
 }
 
 blogSection.append(blogEntries);
